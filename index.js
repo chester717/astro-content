384ba8653a36c04d67d02b5341080c39d075ef07
chester717 <chesterhengsk@gmail.com>
2024-11-17 11:31:49 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -4,12 +4,12 @@
 const apiURL = "https://contenthub-api.eco.astro.com.my/channel/all.json";
 
 const formatTime = (dateTime) => {
-  const date = new Date(dateTime)
+  const date = new Date(dateTime);
 
-  let hours = date.getHours()
-  let minutes = date.getMinutes()
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
 
-  const day = hours >= 12 ? "PM" : 'AM';
+  const day = hours >= 12 ? "PM" : "AM";
 
   hours = hours % 12 || 12;
 
@@ -22,17 +22,15 @@ const getAstroContentGuide = async () => {
   try {
     const response = await fetch(apiURL);
     const data = await response.json();
-    const channels = data.response;
-
+    const channels = data.response; // array of channels data
 
     const channelGrid = document.getElementById("channelGrid");
 
     channels.forEach((channel) => {
       const card = document.createElement("div"); // contain all the channel data
-      card.className = "channel-card"
+      card.className = "channel-card";
 
       const currentSchedule = channel?.currentSchedule || [];
-
 
       const logo = document.createElement("img"); // creating the image
       logo.className = "channel-logo";
@@ -44,13 +42,15 @@ const getAstroContentGuide = async () => {
       info.innerHTML = `
       <div class="channel-number" > CH${channel.stbNumber}</div>
       <div class="channel-title" > ${channel.title}</div>
-      <div class="playing-now" > On Now ${currentSchedule[0].title}</div>;
-      <div class="next" > ${formatTime(currentSchedule[1].dateTime)} ${currentSchedule[1].title}</div>;
-      <div class="later" > ${formatTime(currentSchedule[2].dateTime)} ${currentSchedule[2].title}</div>;
-      `;
+      <div class="playing-now" > On Now ${currentSchedule[0].title}</div>
+      <div class="playing-next" > ${formatTime(currentSchedule[1].datetime)} ${
+        currentSchedule[1].title
+      }</div>
+      <div class="playing-later" > ${formatTime(currentSchedule[2].datetime)} ${
+        currentSchedule[2].title
+      }</div>
 
-      const programList = document.createElement("div");
-      programList.className = "program-list";
+      `;
 
       card.appendChild(logo);
       card.appendChild(info);
